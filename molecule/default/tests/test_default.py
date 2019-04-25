74a8ab254597d54a9a07dd4f72fa53523daab2dd
Ryan Chouinard <rchouinard@gmail.com>
2019-04-25 15:38:20 -0400

    Update molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,19 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nginx_installed(host):
-    pkg = host.package('nginx')
+def test_package_installed(host):
+    pkg = host.package("nginx")
 
     assert pkg.is_installed
 
 
-def test_nginx_config_valid(host):
-    cmd = host.run('nginx -t -c /etc/nginx/nginx.conf')
+def test_service_running(host):
+    srvc = host.service("nginx.service")
 
-    assert cmd.rc == 0
-
-
-def test_nginx_running(host):
-    svc = host.service('nginx')
-
-    assert svc.is_enabled and svc.is_running
+    assert srvc.is_enabled
+    assert srvc.is_running
